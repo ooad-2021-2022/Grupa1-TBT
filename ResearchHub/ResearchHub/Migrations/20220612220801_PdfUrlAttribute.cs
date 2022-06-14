e8214f80959095e8d1485bc180023a2ea44b1091
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-06-14 04:25:03 +0200

    Made multiple migrations
    Some test migrations and migrations for adding new elements in model

---
+++ ResearchHub/ResearchHub/Migrations/20220612220801_PdfUrlAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ResearchHub.Migrations
+{
+    public partial class PdfUrlAttribute : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "pdfFileUrl",
+                table: "ResearchPaper",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "pdfFileUrl",
+                table: "ResearchPaper");
+        }
+    }
+}
